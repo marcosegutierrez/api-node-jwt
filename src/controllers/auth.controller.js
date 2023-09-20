411d53bbad53309112ad2a5549573721c9239b98
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2023-09-19 22:48:41 -0300

    Auth de signUp

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,15 @@
 import User from '../models/User';
 
 export const signUp = async (req, res) => {
+    const { username, email, password, roles } = req.body;
+    
+    const newUser = new User({
+        username,
+        email,
+        password: await User.encryptPassword(password)
+    });
+    console.log(newUser)
+
     res.json("signup");
 }
 
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new Schema ({
     username: {
@@ -22,4 +23,13 @@ const userSchema = new Schema ({
     versionKey: false
 });
 
-export default userSchema;
+userSchema.statics.encryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(11);
+    return await bcrypt.hash(password, salt);
+}
+
+userSchema.statics.comparePasword = async (password, receivedPassword) => {
+    return await bcrypt.compare(password, receivedPassword);
+}
+
+export default model('User', userSchema);
