1438996dc10ce8eac6d8ed33be86e1cd3ccf0245
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2023-09-21 22:19:00 -0300

    Asignacion de Roles a Users

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/User';
 import jwt from 'jsonwebtoken';
+import Role from '../models/Role';
 require('dotenv').config();
 
 export const signUp = async (req, res) => {
@@ -11,6 +12,15 @@ export const signUp = async (req, res) => {
         password: await User.encryptPassword(password)
     });
 
+    if (roles) {
+        const foundRoles = await Role.find({name: {$in: roles}});
+        newUser.roles = foundRoles.map(role => role._id);
+    } else {
+        const role = await Role.findOne({name: "user"});
+        newUser.roles = [role._id];
+    }
+
+    console.log(newUser);
     const savedUser = await newUser.save();
 
     const token = jwt.sign({id: savedUser._id}, process.env.SECRET_KEY, {
