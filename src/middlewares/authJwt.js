929150749973d4631cf28fa61910aead7e79547d
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2023-09-27 20:47:15 -0300

    Autenticacion rol Admin

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -34,3 +34,15 @@ export const isModerator = async (req, res, next) => {
     }
     return res.status(403).json({message: "Require Moderator role"});
 }
+
+export const isAdmin = async (req, res, next) => {
+    const user = await User.findById(req.userId);
+    const roles = await Role.find({_id: {$in: user.roles}});
+    for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "admin") {
+            next();
+            return;
+        }
+    }
+    return res.status(403).json({message: "Require Admin role"});
+}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/', getProducts);
 router.post('/', [ authJwt.verifyToken, authJwt.isModerator ], createProduct);
 router.get('/:id', getProductById);
-router.patch('/:id', authJwt.verifyToken, updateProductById);
-router.delete('/:id', authJwt.verifyToken, deleteProductById);
+router.patch('/:id', [ authJwt.verifyToken, authJwt.isAdmin ], updateProductById);
+router.delete('/:id', [ authJwt.verifyToken, authJwt.isAdmin ], deleteProductById);
 
 export default router;
